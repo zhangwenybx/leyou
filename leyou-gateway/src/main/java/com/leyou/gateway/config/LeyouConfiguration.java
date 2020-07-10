541987c68d173be51155e64521767ed601c7ca42
yaowen <2848790115@qq.com>
2020-07-10 18:43:25 +0800

    跟近

---
+++ leyou-gateway/src/main/java/com/leyou/gateway/config/LeyouConfiguration.java
@@ -0,0 +1,29 @@
+package com.leyou.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class LeyouConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        //初始化cors配置对象
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://manage.leyou.com");
+        corsConfiguration.setAllowCredentials(true); //如果要允许携带cookie，必须指定具体域名
+        corsConfiguration.addAllowedMethod("*"); //*代表所有请求方式
+        corsConfiguration.addAllowedHeader("*");
+
+        //初始化cors配置源对象
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        //返回CorsFilter实例
+        return  new CorsFilter(corsConfigurationSource);
+    }
+}
---
+++ leyou-upload/src/main/java/com/leyou/LeyouUploadApplicaiton.java
@@ -0,0 +1,15 @@
+package com.leyou;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LeyouUploadApplicaiton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouUploadApplicaiton.class);
+    }
+}
